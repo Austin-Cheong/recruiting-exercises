9f5f9003499cc749da294305d2f1ba2ddaa5c440
Austin <aucheong@ucsd.edu>
2020-08-31 00:53:13 -0700

    removed file I/O. Added unit tests using pytest.

--- inventory-allocator/shipments.py
+++ inventory-allocator/shipments.py
@@ -1,30 +1,15 @@
-#!/usr/bin/env python3
-import json
+# !/usr/bin/env python3
 
-f = open("inputs.txt", "r")
-open("outputs.txt", "w").close()
-'''
-{ apple: 5, banana: 5, orange: 5 }, [{ name: owd, inventory: { apple: 5, banana: 5 } }, { name: dm, inventory: { apple: 5, orange: 5 } }, { name: test, inventory: { apple: 1, banana: 2, orange: 3, dragonfruit: 4 } }]
-{ apple: 1 }, [{ name: owd, inventory: { apple: 1 } }]
-{ apple: 10 }, [{ name: owd, inventory: { apple: 5 } }, { name: dm, inventory: { apple: 5 }}]
-{ apple: 1 }, [{ name: owd, inventory: { apple: 0 } }]
-{ apple: 2 }, [{ name: owd, inventory: { apple: 1 } }]
-'''
-
-# Make the input to similar format as JSON:
-def prettyInput():
-
+def main(line):
+    orders = {}
+    inventories = {}
     # Make the orders into JSON format
-    # print("Ugly Input = ", line.rstrip())
-
     # Split the input into orders and inventories
     uglyInput = line.split(", [",1)
     uglyOrders = uglyInput[0]
-    # print("Ugly Orders = ", uglyOrders)
     
-    # Remove the first and last curly braces. Then separate each pair of items using split..
+    # Remove the first and last curly braces. Then separate each pair of items using split()
     uglyOrdersSplit = uglyOrders[2:-2].split(", ")
-    # print("Split ugly orders = ", uglyOrdersSplit)
     
     # Add the key-value pairs to the dictionary
     for item in uglyOrdersSplit:        
@@ -33,99 +18,47 @@ def prettyInput():
         val = pair[1]
         orders[key] = int(val)
     
-    # print("DICTIONARY Orders = ", orders)
-    
-#==============================================================================================
-    # Make the inventories into JSON format:
-
-    # Should be a list of dictionaries where each separate dictionary is a warehouse
+    # Make the inventories into JSON format:    
     uglyInventories = uglyInput[1]
-    # print("Ugly Inventory = ", "[" + uglyInventories)
 
     # Do not include newline char and ending ']' bracket
     strippedInventories = uglyInventories.rstrip()[:-1]
-    # print("Ugly Stripped Inventory = ", strippedInventories)
 
     # Separate individual warehouses from the list
     warehouses = strippedInventories.split("}, {") 
-        # TODO: Test with different amounts of warehouses instead of just 2... 
-    # print("Warehouses = ", warehouses)
-    # print("Warehouses 0 = ", warehouses[0] + "}")
-    # print("Warehouses 1 = ", "{" + warehouses[1])
 
     for item in warehouses:
         warehouseInventory = {}
-        # print("Current warehouse = ", item)
+        # Get the name of the warehouse
         name = item.split(", inventory: ")[0].split(": ")[1]
-        # print("Name = ", name)
-
+        # Get the respective inventory of the warehouse
         inventory = item.split(", inventory: ")[1].replace(" } }", " }")
-        strippedInventory = inventory[2:-2]
-        # print("Inventory = ", strippedInventory)
 
-        splitInventory = strippedInventory.split(", ")
-        # print("Split Inventory = ", splitInventory)
+        splitInventory = inventory[2:-2].split(", ")
         for pair in splitInventory:
             key = str(pair.split(": ")[0])
             val = int(pair.split(": ")[1])
-            # print("Key = ", key)
-            # print("Val = ", val)
             warehouseInventory[key] = int(val)
         
-        # inventories = {name : warehouseInventory}
         inventories[name] = warehouseInventory
-    
-        # print("DICTIONARY Inventories = ", inventories)
-        
-def debugPrinting():
-    print("Orders:")
-    for item in orders:
-        print(item, ": ", orders[item])
 
-    print("------------")
-
-    print("Inventories:")
-    for item in inventories:
-        print(item, ": ", inventories[item])
-
-    # print("Test = ", inventories['owd']['apple'])
-
-
-for line in f:
-    orders = {}
-    inventories = {}
-
-    prettyInput()
-
-    # debugPrinting()
-    # print("================================== LINE ENDING ==================================")
-    
-    
-    '''
-    inventories = { name: { inventory } } 
-    '''
-    print("Orders = ", orders)
-    print("Inventories = ", inventories)
-    # print(inventories.keys())
-    # print(inventories.values())
-    placedOrders = []
+    #  Shipments logic start here:    
+    # This list will contain the answer
+    placedOrders = [] 
 
     for item in orders:
         requested = orders[item]
-        print("~~~~~ Item = ", item, " ||| Requested = ", requested, "~~~~~")
-        # answer = [] 
         for wh in inventories:
-            print("Current warehouse name = ", wh)
-            print("Current warehouse ivty = ", inventories[wh])
-            print("Current requested = ", requested)
             if(requested == 0):
                 break
             else:
-                # if(item in inventories[wh] and requested <= inventories[wh][item]): #TODO: Need to see if the requested amount exists across ALL warehouses...
+                # If the desired item exists and is in stock
                 if(item in inventories[wh] and inventories[wh][item] != 0):
                     # If the requested amount is EQUAL or GREATER than the current wh's stock, take all of that stock from the wh
                     if(requested >= inventories[wh][item]):
+                        # Update the amount of the item being requested
                         requested -= inventories[wh][item]
+                       
                         pair = {}
                         stock = {}
                         # Take the full amount of stock from the warehouse:
@@ -138,41 +71,25 @@ for line in f:
                 
                     # If the requested amount is LESS than the current wh's stock, take just the requested amount, then reduce requested to 0
                     else:
-                        remainingStock = inventories[wh][item] - requested
-                        inventories[wh][item] = remainingStock
-                        # requested -= (inventories[wh][item] - remainingStock) # 5 - (5) = 0
+                        # Update the amount of the item being requested
+                        inventories[wh][item] -= requested
+
                         pair = {}
                         stock = {}
+                         # Take the requested amount of stock from the warehouse:
                         stock[item] = requested
                         pair[wh] = stock
                         placedOrders.append(pair)
                         
-                        # The requested amount is reduced to 0 since it can be fulfilled by this current wh
+                        # The requested amount is reduced to 0 since it can be completely fulfilled by this current wh
                         requested = 0
 
-            print("ANSWERS IN PROGRESS = ", placedOrders)
-        print("REMAINING = ", requested)
-        output = open("outputs.txt", "a")
-        if(requested > 0):
+        # If there is some item requested remaining, then it can't be completely fulfilled by all of the warehouses, so the output is empty
+        if(requested > 0): 
             placedOrders = []
-            break
+            break 
 
-        # for subItem in answer:
-        #     placedOrders.append(subItem)
-            
-    print("REMAINING = ", requested)
-    output = open("outputs.txt", "a")
-    if(requested <= 0):
-        print("ANSWERS = ", placedOrders)
-        formatOutput = str(placedOrders).replace('\'', '').replace('{', '{ ').replace('}',' }')
-        output.write(formatOutput + '\n')
-    else:
-        # print("REQUESTED COULD NOT BE FULLFILLED...")
-        # print("ANSWERS = ", answer)
-        print([])
-        output.write(str([]) + '\n' )
-    output.close()
-
-    print("================================== LINE ENDING ==================================")
-
-f.close()
+    # Make the output similar to the format of the input (only applicable to nonempty list):
+    formatOutput = str(placedOrders).replace('\'', '').replace('{', '{ ').replace('}',' }')
+    
+    return formatOutput
---
+++ inventory-allocator/test_shipments.py
@@ -0,0 +1,42 @@
+from shipments import main
+
+# Order can be shipped using one warehouse
+def test_case1():
+    testInput = '{ apple: 1 }, [{ name: owd, inventory: { apple: 1 } }]'
+    assert main(testInput) == '[{ owd: { apple: 1 } }]'
+
+# Order can be shipped using multiple warehouses
+def test_case2():
+    testInput = '{ apple: 10 }, [{ name: owd, inventory: { apple: 5 } }, { name: dm, inventory: { apple: 5 }}]'
+    assert main(testInput) == '[{ dm: { apple: 5 }}, { owd: { apple: 5 } }]'
+    '''
+    I actually think a more appropriate output would be 
+        '[{ owd: { apple: 5 }}, { dm: { apple: 5 } }]' 
+    since warehouse 'owd' appears before warehouse 'dm' in the input...
+    '''
+
+# Order cannot be shipped because there is not enough inventory v1
+def test_case3():
+    testInput = '{ apple: 1 }, [{ name: owd, inventory: { apple: 0 } }]'
+    assert main(testInput) == '[]'
+
+# Order cannot be shipped because there is not enough inventory v2
+def test_case4():
+    testInput = '{ apple: 2 }, [{ name: owd, inventory: { apple: 1 } }]'
+    assert main(testInput) == '[]'
+
+def test_case5():
+    testInput = '{ apple: 10 }, [{ name: case5a, inventory: { apple: 5 } }, { name: case5b, inventory: { apple: 6 } }]'
+    assert main(testInput) == '[{ case5a: { apple: 5 } }, { case5b: { apple: 5 } }]'
+
+def test_case6():
+    testInput = '{ apple: 10 }, [{ name: case6a, inventory: { apple: 11 } }, { name: case6b, inventory: { apple: 2 } }]'
+    assert main(testInput) == '[{ case6a: { apple: 10 } }]'
+
+def test_case7():
+    testInput = '{ apple: 5, banana: 5, orange: 5 }, [{ name: case7a, inventory: { apple: 5, banana: 5 } }, { name: case7b, inventory: { apple: 5, orange: 5 } }, { name: case7c, inventory: { apple: 1, banana: 2, orange: 3, dragonfruit: 4 } }]'
+    assert main(testInput) == '[{ case7a: { apple: 5 } }, { case7a: { banana: 5 } }, { case7b: { orange: 5 } }]'
+
+def test_case8():
+    testInput = '{ apple: 1, banana: 2, carrot: 3, strawberries: 4, oatmeal: 5, peanut butter: 6 }, [{ name: case6a, inventory: { apple: 2 } }, { name: case6b, inventory: { banana: 1 } }, { name: case6c, inventory: { carrot: 3 } }, { name: case6d, inventory: { strawberries: 4 } }, { name: case6e, inventory: { oatmeal: 5 } }, { name: case6f, inventory: { peanut butter: 6 } }]'
+    assert main(testInput) == '[]'
